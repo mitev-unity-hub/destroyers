da996ce5c0c7d4a84bc7e08b8c9d3cfc883cedb5
o0pitev <pmitev89@gmail.com>
2021-02-19 06:05:41 +0100

    Launch the ball

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,6 +5,9 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
   [SerializeField] private Rigidbody2D currentBallRigidbody; // SerializeField makes it available in the Unity inspektor1
+  [SerializeField] private SpringJoint2D currentSPringJoint;
+
+  [SerializeField] private float delayInSeconds;
   private Camera mainCamera; // declaring a variable mainCamera with type Camera
   private bool isDragging; // are we dragging the ball 
   // Start is called before the first frame update
@@ -21,15 +24,45 @@ public class BallHandler : MonoBehaviour
   void Update()
   {
     if (currentBallRigidbody == null) { return; } // if there is no current ball return
+
     if (!Touchscreen.current.primaryTouch.press.isPressed) // if the player not touching the screen return;
     {
-      currentBallRigidbody.isKinematic = false; // return it back to dinamic so the gravity and spring joint can be active again
+
+      if (isDragging)
+      {
+        LaunchBall();
+      }
+
+      isDragging = false;
       return;
     }
+
+    isDragging = true;
     currentBallRigidbody.isKinematic = true; // while is in kinematic phisics like spring joint and gravity wont be acting on the object
+
     Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue(); // return a Vector 2 value
+
     Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition); // convert pixels to unity(world) units
+
     // Debug.Log(worldPosition);
+
     currentBallRigidbody.position = worldPosition;
+  }
+
+  private void LaunchBall()
+  {
+    currentBallRigidbody.isKinematic = false; // return it back to dinamic so the gravity and spring joint can be active again
+    currentBallRigidbody = null; // clear that entire reference compleately 
+
+    //! the invoke method is needed so it can launch the DetachBall method after sertan amount of time in order the springjoint to launch the ball 
+    //? nameof makes a reference to the method so you get an error if you misspelled it 
+    Invoke(nameof(DetachBall), delayInSeconds);
+
+  }
+
+  private void DetachBall()
+  {
+    currentSPringJoint.enabled = false;
+    currentBallRigidbody = null;
   }
 }
